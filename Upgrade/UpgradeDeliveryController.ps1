2dee2f3e1503b1c755bba9f1bea6264507d0d2fe
Kasper Johansen <37331486+kaspersmjohansen@users.noreply.github.com>
2022-03-24 20:30:46 +0100

    Create UpgradeDeliveryController.ps1

---
+++ Upgrade/UpgradeDeliveryController.ps1
@@ -0,0 +1,14 @@
+﻿$Path = "\\johansen.local\public\Work\Software\Citrix"
+$Product = "Citrix Virtual Apps and Desktops"
+$Version = "2203"
+
+$CVAD = $Path+"\"+$Product+" "+$Version
+$ISO = (Get-ChildItem -Path "$CVAD\*" -Include *.iso).Name
+
+Mount-DiskImage -ImagePath "$CVAD\$ISO"
+
+$InstallerSwitches = "/components CONTROLLER,DESKTOPDIRECTOR /ignore_hw_check_failure /noreboot /passive"
+
+$Volume = Get-Volume | where {$_.FileSystem -like "UDF" -and $_.FileSystemLabel -like "CVAD*"}
+$DrvLtr = $Volume.Driveletter+":"
+Start-Process -FilePath "$DrvLtr\x64\XenDesktop Setup\XenDesktopServerSetup.exe" -ArgumentList $InstallerSwitches -Wait
