9770c504d1cad8dd4514f8d7d30a495975ec44a6
Kasper Johansen <37331486+kaspersmjohansen@users.noreply.github.com>
2022-06-30 08:41:08 +0200

    Updated the CVAD version information

--- Upgrade/UpgradeDeliveryController.ps1
+++ Upgrade/UpgradeDeliveryController.ps1
@@ -1,6 +1,6 @@
-﻿$Path = ""
+﻿$Path = "\\johansen.local\public\Work\Software\Citrix"
 $Product = "Citrix Virtual Apps and Desktops"
-$Version = "2203"
+$Version = "2206"
 
 $CVAD = $Path+"\"+$Product+" "+$Version
 $ISO = (Get-ChildItem -Path "$CVAD\*" -Include *.iso).Name
@@ -12,3 +12,4 @@ $InstallerSwitches = "/components CONTROLLER,DESKTOPDIRECTOR /ignore_hw_check_fa
 $Volume = Get-Volume | where {$_.FileSystem -like "UDF" -and $_.FileSystemLabel -like "CVAD*"}
 $DrvLtr = $Volume.Driveletter+":"
 Start-Process -FilePath "$DrvLtr\x64\XenDesktop Setup\XenDesktopServerSetup.exe" -ArgumentList $InstallerSwitches -Wait
+Dismount-DiskImage -ImagePath "$CVAD\$ISO"
